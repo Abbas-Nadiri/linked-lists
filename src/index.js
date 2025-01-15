8caac84c6c27a8527dd3d14dc7be47189783e8a6
ahnadiri <108028111+Abbas-Nadiri@users.noreply.github.com>
2025-01-15 18:41:15 +0000

    add at, pop, find, toString, insertAt, and removeAt methods to LinkedList class

--- src/index.js
+++ src/index.js
@@ -1,39 +1,137 @@
-class Node{
-    constructor(value = null, nextNode = null) {
-        this.value = value;
-        this.nextNode = nextNode;
-    }
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
 }
 
 class LinkedList {
-    constructor() {
-        this.head = null;
-        this.length = 0;
+  constructor() {
+    this.headNode = null;
+    this.length = 0;
+  }
+  append(value) {
+    const newNode = new Node(value);
+    if (!this.headNode) {
+      this.headNode = newNode;
+    } else {
+      let current = this.headNode;
+      while (current.nextNode) {
+        current = current.nextNode;
+      }
+      current.nextNode = newNode;
     }
-    append(value) {
-        const newNode = new Node(value);
-        if (!this.head) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            while (current.nextNode) {
-                current = current.nextNode;
-            };
-            current.nextNode = newNode;
-        }
-        this.length++;
+    this.length++;
+  }
+  prepend(value) {
+    this.headNode = new Node(value, this.headNode);
+    this.length++;
+  }
+  size() {
+    return this.length;
+  }
+  head() {
+    return this.headNode;
+  }
+  tail() {
+    let current = this.headNode;
+    if (!current) return null;
+    while (current?.nextNode) {
+      current = current.nextNode;
     }
-    prepend(value) {
-        this.head = new Node(value, this.head);
-        this.length++;
+    return current;
+  }
+  at(index) {
+    if (index > this.length || index < 0 || !Number.isInteger(index))
+      return 'Index invalid/out of bounds';
+    let current = this.headNode;
+    while (index) {
+      current = current.nextNode;
+      index--;
     }
-
+    return current;
+  }
+  pop() {
+    let current = this.headNode;
+    if (!current) return null;
+    while (current?.nextNode.nextNode) {
+      current = current.nextNode;
+    }
+    current.nextNode = null;
+  }
+  contains(value) {
+    let current = this.headNode;
+    if (!current) return false;
+    while (current) {
+      if (current.value == value) {
+        return true;
+      }
+      current = current.nextNode;
+    }
+    return false;
+  }
+  find(value) {
+    let current = this.headNode;
+    let position = 0;
+    if (!current) return null;
+    while (current) {
+      if (current.value == value) {
+        return position;
+      }
+      current = current.nextNode;
+      position++;
+    }
+    return null;
+  }
+  toString() {
+    let string = '';
+    let current = this.headNode;
+    while (current) {
+      string = string + `( ${current.value} ) -> `;
+      current = current.nextNode;
+    }
+    string = string + 'null';
+    return string;
+  }
+  insertAt(value, index) {
+    if (index > this.length || index < 0 || !Number.isInteger(index)) {
+      return console.log('Index invalid/out of bounds');
+    }
+    let newNode = new Node(value);
+    if (index == 0) {
+      let currentHead = this.headNode;
+      newNode.nextNode = currentHead;
+      this.headNode = newNode;
+    }
+    let current = this.headNode;
+    while (index > 1) {
+      current = current.nextNode;
+      index--;
+    }
+    let newNodeFollowing = current.nextNode;
+    current.nextNode = newNode;
+    newNode.nextNode = newNodeFollowing;
+  }
+  removeAt(index) {
+    if (index > this.length || index < 0 || !Number.isInteger(index)) {
+      return console.log('Index invalid/out of bounds');
+    }
+    let current = this.headNode;
+    if (index == 0) this.headNode = current.nextNode;
+    while (index > 1) {
+      current = current.nextNode;
+      index--;
+    }
+    let preceding = current;
+    let following = current.nextNode?.nextNode;
+    preceding.nextNode = following;
+  }
 }
 
-
-
-const linkedList = new LinkedList();
-linkedList.append("first");
-linkedList.append("second");
-linkedList.append("third");
-console.log(linkedList.head);
+const list = new LinkedList();
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
